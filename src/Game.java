443faa5b8b5ab3b209ed3c8a0a89e7138650d1ed
“madeleineMU24” <3tomu24.thunma@folkuniversitetet.nu>
2024-11-18 15:31:29 +0100

    test

---
+++ src/Game.java
@@ -0,0 +1,19 @@
+public class Game {
+
+    public Game(){
+        board();
+    }
+
+
+    public void board(){
+
+        System.out.println("     " + "|" + "     " + "|" + "     ");
+        System.out.println("-----+-----+-----");
+        System.out.println("     " + "|" + "     " + "|" + "     ");
+        System.out.println("-----+-----+-----");
+        System.out.println("     " + "|" + "     " + "|" + "     ");
+
+    }
+
+
+}
